877a21e7a5c80cf1ea462fbba612269851b6a5bb
Marc Neudert <marc.neudert@gmail.com>
2018-10-14 15:04:37 +0200

    wires up ref_inspector call

--- lib/ref_inspector/plug.ex
+++ lib/ref_inspector/plug.ex
@@ -3,9 +3,25 @@ defmodule RefInspector.Plug do
   RefInspector Plug
   """
 
+  import Plug.Conn
+
   @behaviour Plug
 
   def init(opts), do: opts
 
-  def call(conn, _opts), do: conn
+  def call(conn, _opts) do
+    lookup =
+      case get_req_header(conn, "referer") do
+        [] -> RefInspector.parse(nil)
+        [referer | _] -> RefInspector.parse(referer)
+      end
+
+    put_private(conn, :ref_inspector, lookup)
+  end
+
+  @doc """
+  Returns the lookup result from the connection.
+  """
+  @spec get_result(Plug.Conn.t()) :: nil | RefInspector.Result.t()
+  def get_result(conn), do: conn.private[:ref_inspector]
 end
--- test/ref_inspector/plug_test.exs
+++ test/ref_inspector/plug_test.exs
@@ -2,6 +2,8 @@ defmodule RefInspector.PlugTest do
   use ExUnit.Case
   use Plug.Test
 
+  @referer "http://www.google.com/search?q=ref_inspector"
+
   defmodule Router do
     use Plug.Router
 
@@ -15,7 +17,24 @@ defmodule RefInspector.PlugTest do
 
   @opts Router.init([])
 
-  test "returns 200" do
-    assert 200 == conn(:get, "/") |> Router.call(@opts) |> Map.fetch!(:status)
+  test "empty result for empty referer" do
+    conn = conn(:get, "/") |> Router.call(@opts)
+
+    assert 200 == conn.status
+    assert %RefInspector.Result{referer: ""} == RefInspector.Plug.get_result(conn)
+  end
+
+  test "result for referer" do
+    conn =
+      conn(:get, "/")
+      |> put_req_header("referer", @referer)
+      |> Router.call(@opts)
+
+    assert 200 == conn.status
+
+    assert %RefInspector.Result{
+             referer: @referer,
+             source: "Google"
+           } = RefInspector.Plug.get_result(conn)
   end
 end
