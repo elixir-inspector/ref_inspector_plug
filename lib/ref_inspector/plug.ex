34249c855d5eb8602a8d567360c934bbfc4934cd
Marc Neudert <marc.neudert@gmail.com>
2018-11-25 12:34:12 +0100

    doc

--- lib/ref_inspector/plug.ex
+++ lib/ref_inspector/plug.ex
@@ -1,6 +1,37 @@
 defmodule RefInspector.Plug do
   @moduledoc """
   RefInspector Plug
+
+  ## Usage
+
+  After ensuring `:ref_inspector` is configured you need to add the plug:
+
+      defmodule MyRouter do
+        use Plug.Router
+
+        # ...
+        plug RefInspector.Plug
+        # ...
+
+        plug :match
+        plug :dispatch
+      end
+
+  Depending on how you are using plugs the actual location may vary.
+  Please consult your frameworks documentation to find the proper place.
+
+  Once setup the connection will be automatically enriched with the results of
+  a lookup based on the connections `referer` header:
+
+      defmodule MyRouter do
+        get "/" do
+          case RefInspector.Plug.get_result(conn) do
+            %{referer: ""} -> send_resp(conn, 404, "Missing referer")
+            %{source: :unknown} -> send_resp(conn, 200, "Unknown referer")
+            %{source: source} -> send_resp(conn, 200, "Client source: " <> source)
+          end
+        end
+      end
   """
 
   import Plug.Conn
