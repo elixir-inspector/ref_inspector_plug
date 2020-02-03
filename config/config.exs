ade3be0dc7bb8deb3309c4e7468b39b18a5bf630
Marc Neudert <marc.neudert@gmail.com>
2020-02-03 18:07:38 +0100

    Use referers.yml from GitHub master for tests

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@ use Mix.Config
 if Mix.env() == :test do
   config :ref_inspector,
     database_path: Path.expand("../test/database", __DIR__),
+    remote_urls: [{"referers.yml", "https://raw.githubusercontent.com/snowplow-referer-parser/referer-parser/master/resources/referers.yml"}],
     startup_silent: true,
     startup_sync: true
 end
