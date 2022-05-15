ef5af37e7f690fdd9a4b5e52de77870e7e2ea2c1
Marc Neudert <marc.neudert@gmail.com>
2022-05-15 12:01:14 +0200

    Remove unnecessary :plt_add_apps config

--- mix.exs
+++ mix.exs
@@ -44,7 +44,6 @@ defmodule RefInspector.Plug.MixProject do
         :underspecs,
         :unmatched_returns
       ],
-      plt_add_apps: [:mix],
       plt_core_path: "plts",
       plt_local_path: "plts"
     ]
